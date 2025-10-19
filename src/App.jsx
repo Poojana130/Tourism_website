cdc5494ab60b9b92123ade34c703c1f5004febc8
Poojana130 <spoojana10@gmail.com>
2025-10-19 13:12:49 +0530

    Auto update

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,20 @@
 // import { useState } from 'react'
 // import reactLogo from './assets/react.svg'
 // import viteLogo from '/vite.svg'
-import './App.css'
-// import HomePage from './components/HomePage'
+import './App.css';
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import HomePage from './components/Home Page/HomePage'
 import Navbar from './components/Navbar/Navbar'
 
 function App() {
   
 
   return (
-    <div>
-    {/* <HomePage></HomePage> */}
-    <Navbar/>
-    </div>
+
+  <Router>
+      <Navbar/>
+    <Routes path='/' element ={<HomePage/>}/>
+  </Router>
   )
 }
 
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,32 +1,49 @@
-import React, { Component } from "react";
+import React, { useState } from "react";
+import './Navbar.css';
+import { IoIosSearch } from "react-icons/io";
+import { FaBars, FaTimes } from "react-icons/fa";
 
-class Navbar extends Component
-{
-     render()
-     {
-          return(
-               <section className="navbar">
+export default function Navbar() {
+  const [menuOpen, setMenuOpen] = useState(false);
 
-                    <div className="navigation">
+  return (
+    <section className="navbar">
+      <div className="navigation">
 
-                         <div>
+        {/* Logo */}
+        <div className="logo">
+          <h3>Journey Through Ages</h3>
+        </div>
 
-                         </div>
+        {/* Hamburger Icon */}
+        <div className="menu-icon" onClick={() => setMenuOpen(!menuOpen)}>
+          {menuOpen ? <FaTimes /> : <FaBars />}
+        </div>
 
-                         <div>
+        {/* Menu Content (Links + Search + Button) */}
+        <div className={`menu-content ${menuOpen ? "active" : ""}`}>
+          <ul className="menu-links">
+            <li>Home</li>
+            <li>Explore</li>
+            <li>Experiences</li>
+            <li>Travel Guides</li>
+            <li>Blog</li>
+            <li>About India</li>
+          </ul>
 
-                         </div>
+          <div className="menu-search">
+            <div className="search-box">
+              <IoIosSearch />
+              <input type="text" placeholder="Search" />
+            </div>
 
-                         <div>
-                              
-                         </div>
+            <div className="explore">
+              <button>Start Exploring</button>
+            </div>
+          </div>
+        </div>
 
-                    </div>
-
-                    
-               </section>
-          )
-     }
-};
-
-export default Navbar;
+      </div>
+    </section>
+  );
+}
