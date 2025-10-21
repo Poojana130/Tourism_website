f3d62daabdd2c8f2cfb91956f2e4a016564fd31f
Poojana130 <spoojana10@gmail.com>
2025-10-21 14:12:40 +0530

    Auto update

--- src/components/Home Page/HomePage.jsx
+++ src/components/Home Page/HomePage.jsx
@@ -6,8 +6,12 @@ import Spiritual from '../../assets/Images/Spiritual.svg';
 import Food from '../../assets/Images/Food.svg';
 import Rajasthan from '../../assets/Images/Rajasthan.svg';
 import Kerala from '../../assets/Images/Kerala.svg';
-import Goa from '../../assets/Images/Goa.png~';
+import Goa from '../../assets/Images/Goa.png';
 import Karnataka from '../../assets/Images/Karnataka.svg';
+import Heritage1 from '../../assets/Images/Heritage.png';
+import Spiritual1 from '../../assets/Images/Spiritual.png';
+import Nature1 from '../../assets/Images/Nature.png';
+import Adventure1 from '../../assets/Images/Adventures.png';
 
 class HomePage extends Component
 {
@@ -33,6 +37,33 @@ class HomePage extends Component
       name: "Karnataka",
       tagline: "Land Of Ancient Temples",
       image: Karnataka,
+    },
+  ];
+
+  const themes = [
+    {
+      image: Heritage1,
+      title: "Heritage",
+      description:
+        "Step into India’s Regal Past Through Its Majestic Forts, Palaces & Ancient Ruins. Experience The Tales of Glory, Empire & Timeless Artistry.",
+    },
+    {
+      image: Nature1,
+      title: "Nature",
+      description:
+        "From The Misty Himalayas To The Serene Backwaters And Golden Deserts, India’s Landscape Has No Parallel. Explore Nature In Every Form — Tropical & Breathtakingly Beautiful.",
+    },
+    {
+      image: Spiritual1,
+      title: "Spiritual",
+      description:
+        "Journey Through Sacred Temples, Mosques, Churches, And Monasteries. Seek Peace, Positive Vibes, Devotion, And The Spiritual Energy That Defines A Diverse Nation.",
+    },
+    {
+      image: Adventure1,
+      title: "Adventure",
+      description:
+        "Feel The Thrill With Activities Like Trekking, Rafting, Skydiving & More — Adventure Awaits For The Brave & Curious.",
     },
   ];
           return(
@@ -90,6 +121,31 @@ class HomePage extends Component
           </div>
         </div>
       ))}
+    </div>
+
+    <div className="homepagefea">
+     <h3>Explore by Theme</h3>
+
+     <div className="themes-container">
+        {themes.map((theme, index) => (
+          <div className="theme-card" key={index}>
+            <div className="theme-image">
+              <img src={theme.image} alt={theme.title} />
+            </div>
+            <div className="theme-content">
+       <h3>{theme.title}</h3>
+            <p>{theme.description}</p>
+            <button>Start Exploring</button>
+            </div>
+            
+          </div>
+        ))}
+      </div>
+
+    </div>
+
+    <div className="seasonal">
+     <h3>Seasonal Suggestions</h3>
     </div>
 
                     
